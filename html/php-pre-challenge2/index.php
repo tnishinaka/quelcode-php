b15efba7ef57f9b294fb2bd24bbe07b4ead48472
tnishinaka <60943055+tnishinaka@users.noreply.github.com>
2020-05-04 19:29:26 +0900

    練習課題２

--- html/php-pre-challenge2/index.php
+++ html/php-pre-challenge2/index.php
@@ -3,10 +3,15 @@ $array = explode(',', $_GET['array']);
 
 // 修正はここから
 for ($i = 0; $i < count($array); $i++) {
-
+    for ($j=1; $j < count($array); $j++) { 
+        if($array[$j-1] > $array[$j]) {
+            $temp = $array[$j];
+            $array[$j] = $array[$j-1];
+            $array[$j-1] = $temp;
+        }
+    }
 }
 // 修正はここまで
-
 echo "<pre>";
 print_r($array);
 echo "</pre>";
