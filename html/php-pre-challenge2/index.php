6fae14f805286b968eadb88578ab4b4d11da7c9d
tnishinaka <60943055+tnishinaka@users.noreply.github.com>
2020-05-07 13:44:18 +0900

    上限の変数を分ける

--- html/php-pre-challenge2/index.php
+++ html/php-pre-challenge2/index.php
@@ -2,8 +2,10 @@
 $array = explode(',', $_GET['array']);
 
 // 修正はここから
-for ($i = 0; $i < count($array); $i++) {
-    for ($j=1; $j < count($array); $j++) { 
+$length = count($array);
+for ($i = 0; $i < $length; $i++) {
+    $limit = $length - $i;
+    for ($j=1; $j < $limit; $j++) { 
         if($array[$j-1] > $array[$j]) {
             $temp = $array[$j];
             $array[$j] = $array[$j-1];
