3511314148cedaa78e2ea3afa49b1b3d5129d2f0
tnishinaka <60943055+tnishinaka@users.noreply.github.com>
2020-05-11 01:51:52 +0900

    init.sql add

--- mysql5.7/init.sql
+++ mysql5.7/init.sql
@@ -11,6 +11,7 @@ CREATE TABLE `members` (
 
 CREATE TABLE `posts` (
       `id` int(11) NOT NULL AUTO_INCREMENT,
+      `uniqueness_id` int(11) NOT NULL,
       `message` text NOT NULL,
       `member_id` int(11) NOT NULL,
       `reply_post_id` int(11) NOT NULL,
@@ -18,6 +19,16 @@ CREATE TABLE `posts` (
       `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
       PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
+
+CREATE TABLE `posts_option` (
+      `member_id` int(11) NOT NULL,
+      `post_id` int(11) NOT NULL,
+      `uniqueness_id` int(11) NOT NULL,
+      `favorite` int(11) NOT NULL DEFAULT 0,
+      `retweet` int(11) NOT NULL DEFAULT 0,
+      `created` datetime NOT NULL,
+      `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 CREATE TABLE `prechallenge3` (
       `value` int(11) NOT NULL
